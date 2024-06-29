1e5a240c98b164703998541d94e8e254730075c8
Ankit.exe <ankitghimire321@gmail.com>
2024-06-29 19:55:19 +0545

    edit comment

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -43,14 +43,33 @@ export const likeComment = async (req: Request, res: Response, next: NextFunctio
         if (userIndex === -1) {
             comment.numberOfLikes += 1;
             comment.likes.push(req.userId)
-        }else{
+        } else {
             comment.numberOfLikes -= 1;
-            comment.likes.splice(userIndex,1)
+            comment.likes.splice(userIndex, 1)
         }
         await comment.save();
         res.status(200).json(comment);
     }
     catch (error) {
         next(error);
+    }
+}
+
+export const editComment = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(res.status(402).send("Comment not found"));
+        }
+        if (comment.userId !== req.userId) {
+            return next(res.status(403).send("You are not allowed to edit this comment"))
+        }
+        const editedComment = await Comment.findByIdAndUpdate(req.params.commentId, {
+            content: req.body.content,
+        }, { new: true }
+        );
+        res.status(200).json(editedComment)
+    } catch (error) {
+        next(error)
     }
 }
--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../middleware/verifyUser';
-import { createComment, getPostComments, likeComment } from '../controllers/comment.controller';
+import { createComment, getPostComments, likeComment, editComment } from '../controllers/comment.controller';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
-router.put('/likeComment/:commentId', verifyToken, likeComment)
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
 
 export default router;
