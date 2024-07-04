50e2a85c68383582ee4f89ce042d86322749109f
Ankit.exe <ankitghimire321@gmail.com>
2024-07-04 22:52:44 +0545

    fix

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://digitalkatha-frontend.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
